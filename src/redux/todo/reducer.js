862d5a02fc37fdeac4fe84c95a8cd747845ee1fd
wahaj ashfaq <wahajashfaq2013@outlook.com>
2022-01-29 03:19:59 +0500

    Latst code

--- src/redux/todo/reducer.js
+++ src/redux/todo/reducer.js
@@ -1,9 +1,11 @@
-const reducer = (
-  state = {
-    items: []
-  },
-  action
-) => {
+const initialState = {
+  items: []
+};
+
+const reducer = (state = initialState, action = {}) => {
+  const newState = {
+    ...state
+  };
   switch (action.type) {
     case "ADD_ITEM": {
       break;
@@ -14,9 +16,11 @@ const reducer = (
     case "ADD_ITEM_ROLLBACK": {
       break;
     }
-    default:
+    default: {
       break;
+    }
   }
+  return newState;
 };
 
 export default reducer;
